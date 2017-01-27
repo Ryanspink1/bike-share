d5f598c224c58f10fdeb04c54f8c956417cb2d0e
Ryan Spink <ryanspinktech@gmail.com>
2017-01-26 19:43:30 -0700

    adds CRUD functionality to controller

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,50 @@
 class BikeShareApp < Sinatra::Base
 
+  get "/" do
+    erb :
+  end
+
+  get "/stations" do
+    @stations = Station.all
+    erb :"stations/index"
+  end
+
+  get "/stations/new" do
+    ## if you want to have the user select city, installation date, and/or block count with a drop down, add instance variables like the one below.
+    ## @cities = City.all.order(:name)
+    erb :"stations/new"
+  end
+
+  post "/stations" do
+    ## if you want to have the user select city, installation date, and/or block count with a text field,
+    ##     you must create a new object or find an existing object with the data the user entered and then replace that text with the id in the parameters to create the station
+    ##     params[:city] = City.find_or_create_by(name: params[:city]).id
+    @station = Station.create(params[:station])
+    redirect "/stations/#{@station.id}"
+  end
+
+  get "/stations/:id" do
+    @station = Station.find(params[:id])
+    erb :"stations/show"
+  end
+
+  get "/stations/:id/edit" do
+    ## if you want to have the user select city, installation date, and/or block count with a drop down, add instance variables like the one below.
+    ## @cities = City.all.order(:name)
+    @station = Station.find(params[:id])
+    erb :"stations/edit"
+  end
+
+  put "/stations/:id" do
+    ## if you want to have the user select city, installation date, and/or block count with a text field,
+    ##     you must create a new object or find an existing object with the data the user entered and then replace that text with the id in the parameters to create the station
+    ##     params[:city] = City.find_or_create_by(name: params[:city]).id
+    @station = Station.update(params[:id],params[:station])
+    redirect "/stations/#{@station.id}"
+  end
+
+  delete "/stations/:id" do
+    @station = Station.destroy(params[:id])
+    redirect "/stations"
+  end
 end

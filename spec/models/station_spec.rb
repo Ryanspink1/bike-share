e5e1d3b3077ce143f771aed53ccd0b2d6584fe3e
Charlotte Moore <mooc123@gmail.com>
2017-01-29 13:58:13 -0700

    updates model tests to remove separate models for InstallationDate and DockCount

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -3,72 +3,129 @@ require_relative '../spec_helper'
 RSpec.describe Station do
   describe 'validations' do
     it 'validates presence of name' do
-      supporting_info
-
       station = Station.create(
-        city: City.find(1),
-        dock_count: DockCount.find(1),
-        installation_date: InstallationDate.find(1)
+        city: City.create(name: "New Victoria"),
+        dock_count: 1,
+        installation_date: 1
       )
 
       expect(station).to_not be_valid
     end
 
     it 'validates presence of city' do
-      supporting_info
-
       station = Station.create(
         name: "Blick & Lock",
-        dock_count: DockCount.find(1),
-        installation_date: InstallationDate.find(1)
+        dock_count: 1,
+        installation_date: 1
       )
 
       expect(station).to_not be_valid
     end
 
     it 'validates presence of dock_count' do
-      supporting_info
-
       station = Station.create(
         name: "Blick & Lock",
-        city: City.find(1),
-        installation_date: InstallationDate.find(1)
+        city: City.create(name: "New Victoria"),
+        installation_date: 1
       )
 
       expect(station).to_not be_valid
     end
 
     it 'validates presence of installation_date' do
-      supporting_info
-
       station = Station.create(
         name: "Blick & Lock",
-        city: City.find(1),
-        dock_count: DockCount.find(1)
+        city: City.create(name: "New Victoria"),
+        dock_count: 1
       )
 
       expect(station).to_not be_valid
     end
 
     it 'is valid with valid parameters' do
-      supporting_info
-
       station = Station.create(
         name: "Blick & Lock",
-        city: City.find(1),
-        dock_count: DockCount.find(1),
-        installation_date: InstallationDate.find(1)
+        city: City.create(name: "New Victoria"),
+        dock_count: 1,
+        installation_date: '1/1/2017'
       )
-
-      binding.pry
 
       expect(station).to be_valid
     end
   end
-end
 
-def supporting_info
-    City.create(name: "New Victoria")
-    DockCount.create(dock_number: 1)
-    InstallationDate.create(installation_date: "1/1/2017")
+  describe 'installation_date format' do
+
+
+    describe 'date import' do
+      it 'imports correctly with single digit date where month and day are the same' do
+        station = Station.create(
+          name: "Blick & Lock",
+          city: City.create(name: "New Victoria"),
+          dock_count: 1,
+          installation_date: "1/1/2017"
+        )
+
+        expect(station.installation_date).to eq(Date.parse("Jan 1 2017"))
+      end
+
+      it 'is valid with valid double digit date where month and day are the same' do
+        station = Station.create(
+          name: "Blick & Lock",
+          city: City.create(name: "New Victoria"),
+          dock_count: 1,
+          installation_date: "10/10/2017"
+        )
+
+        expect(station.installation_date).to eq(Date.parse("Oct 10 2017"))
+      end
+
+      it 'switches month and day when importing valid date in m/d/Y format' do
+        station = Station.create(
+          name: "Blick & Lock",
+          city: City.create(name: "New Victoria"),
+          dock_count: 1,
+          installation_date: "2/4/2017"
+        )
+
+        expect(station.installation_date).to_not eq(Date.parse("Feb 4 2017"))
+      end
+
+      it 'is nil when importing invalid date in m/d/Y format' do
+        station = Station.create(
+          name: "Blick & Lock",
+          city: City.create(name: "New Victoria"),
+          dock_count: 1,
+          installation_date: "2/15/2017"
+        )
+
+        expect(station.installation_date).to eq(nil)
+      end
+
+      it 'is correct when m/d/Y single digit date is parsed before import' do
+        formatted = Date.strptime('2/15/2017', '%m/%d/%Y')
+
+        station = Station.create(
+          name: "Blick & Lock",
+          city: City.create(name: "New Victoria"),
+          dock_count: 1,
+          installation_date: formatted
+        )
+
+        expect(station.installation_date).to eq(Date.parse("Feb 15 2017"))
+      end
+
+      it 'is correct when m/d/Y double digit date is parsed before import' do
+        formatted = Date.strptime('11/30/2017', '%m/%d/%Y')
+        station = Station.create(
+          name: "Blick & Lock",
+          city: City.create(name: "New Victoria"),
+          dock_count: 1,
+          installation_date: formatted
+        )
+
+        expect(station.installation_date).to eq(Date.parse("Nov 30 2017"))
+      end
+    end
+  end
 end

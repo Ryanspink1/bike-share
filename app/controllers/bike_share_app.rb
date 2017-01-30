e0643f987aa940947e82b4e1bba9228cf6d62aa1
Ashley Schauer <aelschauer@gmail.com>
2017-01-30 14:49:45 -0700

    adds html and updates controller for trips/index and trips/edit

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,5 +1,9 @@
 require 'pry'
+require 'will_paginate'
+require 'will_paginate/active_record'
+
 class BikeShareApp < Sinatra::Base
+  include WillPaginate::Sinatra::Helpers
 
   get "/" do
     erb :"home/home"
@@ -15,10 +19,12 @@ class BikeShareApp < Sinatra::Base
   end
 
   post "/stations" do
-    @station = Station.create(name: params[:station][:name],
-     city_id: City.find_or_create_by(params[:city]).id,
-     dock_count: params[:station][:dock_count],
-     installation_date: params[:station][:installation_date])
+    @station = Station.create(
+      name:              params[:station][:name],
+      city_id:           City.find_or_create_by(params[:city]).id,
+      dock_count:        params[:station][:dock_count],
+      installation_date: params[:station][:installation_date]
+    )
     redirect "/stations/#{@station.id}"
   end
 
@@ -33,10 +39,12 @@ class BikeShareApp < Sinatra::Base
   end
 
   put "/stations/:id" do
-    Station.update(name: params[:station][:name],
-     city_id: City.find_or_create_by(params[:city]).id,
-     dock_count: params[:station][:dock_count],
-     installation_date: params[:station][:installation_date])
+    Station.update(params[:id].to_i,
+      name:              params[:station][:name],
+      city_id:           City.find_or_create_by(params[:city]).id,
+      dock_count:        params[:station][:dock_count],
+      installation_date: params[:station][:installation_date]
+    )
 
     @station = Station.find(params[:id])
 
@@ -55,4 +63,61 @@ class BikeShareApp < Sinatra::Base
     erb :"stations/dashboard"
   end
 
+  get "/trips" do
+    @trips = Trip.all.order(start_date: :desc)
+    @trips = @trips.paginate(page: params[:page], per_page: 30)
+    erb :"trips/index"
+  end
+
+  get "/trips/new" do
+    erb :"trips/new"
+  end
+
+  post "/trips" do
+    params["trip"] = Trip.format_parameters(params[:trip])
+    @trip = Trip.create(
+      duration:          params[:trip][:duration],
+      start_date:        params[:trip][:start_date],
+      start_station_id:  params[:trip][:start_station_id],
+      end_date:          params[:trip][:end_date],
+      end_station_id:    params[:trip][:end_station_id],
+      bike_id:           params[:trip][:bike_id],
+      subscription_type: params[:trip][:subscription_type],
+      zip_code:          params[:trip][:zip_code]
+    )
+    redirect "/trips/#{@trip.id}"
+  end
+
+  get "/trips/:id" do
+    @trip = Trip.find(params[:id])
+    erb :"trips/show"
+  end
+
+  get "/trips/:id/edit" do
+    @trip = Trip.find(params[:id])
+    @stations = Station.all
+    @subscription_type_list = Trip.subscription_type_list
+    erb :"trips/edit"
+  end
+
+  put "/trips/:id" do
+    params["trip"] = Trip.format_parameters(params[:trip])
+    Trip.update(params[:id].to_i,
+      duration:          params[:trip][:duration],
+      start_date:        params[:trip][:start_date],
+      start_station_id:  params[:trip][:start_station_id],
+      end_date:          params[:trip][:end_date],
+      end_station_id:    params[:trip][:end_station_id],
+      bike_id:           params[:trip][:bike_id],
+      subscription_type: params[:trip][:subscription_type],
+      zip_code:          params[:trip][:zip_code]
+    )
+
+    redirect "/trips/#{@trip.id}"
+  end
+
+  delete "/trips/:id" do
+    @trip = Trip.destroy(params[:id])
+    redirect "/trips"
+  end
 end
